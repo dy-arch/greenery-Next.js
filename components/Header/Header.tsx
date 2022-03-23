7a7a0056c8b63348a54819169aab1017b6e5028f
dy-arch <lk8969@gmail.com>
2022-03-24 02:24:19 +0900

    Add: Header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Link href="/">
+          <Image src="" alt="" />
+        </Link>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
