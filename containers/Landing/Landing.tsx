a63dc5d1940374322e6b1dcc55bbc592572624b2
dy-arch <lk8969@gmail.com>
2022-03-24 02:23:02 +0900

    Add: Landing container

---
+++ containers/Landing/Landing.tsx
@@ -0,0 +1,18 @@
+import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+import { MainContainer } from "components/MainContainer";
+import { TodayArticleSection } from "./Landing.style";
+
+export function Landing({ article }: any) {
+  return (
+    <>
+      <header></header>
+      <MainContainer>
+        <TodayArticleSection>
+          <ReactMarkdown>{article.content}</ReactMarkdown>
+        </TodayArticleSection>
+        <Link href="/hello">hi</Link>
+      </MainContainer>
+    </>
+  );
+}
