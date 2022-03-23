22632bb049431e1d40a61fe978c4e8e6a802e321
dy-arch <lk8969@gmail.com>
2022-03-24 02:20:47 +0900

    Feat: normalize, theme 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import reset from "styles/GlobalStyles";
+import { Global, ThemeProvider } from "@emotion/react";
+import { theme } from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Global styles={reset} />
       <Head>
         <title>초록친구</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,8 @@
+import type { Theme } from "@emotion/react";
+
+export const theme: Theme = {
+  containerSize: {
+    large: "1200px",
+    small: "720px",
+  },
+};
